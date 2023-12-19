41f1ae4c0f18ee758cb6a7d713dde56f97109ad7
Bagdeev Vladimir <kirog11az@gmail.com>
2023-12-19 17:14:23 +0300

    web build fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
+  
   const config = await createExpoWebpackConfigAsync(env, argv);
-
+  config.output = {
+    publicPath: './'
+  }
   config.module.rules.forEach(rule => {
     if (rule.oneOf instanceof Array) {
       rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/]
